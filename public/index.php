d145bd148da86e24df5e9ae1f5cd88522e8bc7de
devidy <devidy.oliveira@gmail.com>
2021-01-07 16:12:22 -0300

    feat: implementa getters e setters

--- public/index.php
+++ public/index.php
@@ -5,3 +5,6 @@ use App\Lottery\Entity\Lottery;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $lottery = new Lottery(3, 6);
+
+var_dump($lottery->getTotalGames()); 
+var_dump($lottery->getQuantityTens()); 
--- src/Entity/Lottery.php
+++ src/Entity/Lottery.php
@@ -38,7 +38,84 @@ class Lottery
             throw new \Exception('A quantidade de dezenas deve ser entre 6 e 10');
         }
         
-        $this->totalGames = $totalGames;
+        $this->setTotalGames($totalGames);
+        $this->setQuantityTens($quantityTens);
+    }
+
+    /**
+     * 
+     * @param int $quantityTens
+     * @return void
+     */
+    public function setQuantityTens($quantityTens)
+    {
         $this->quantityTens = $quantityTens;
     }
+
+    /**
+     * 
+     * @return int
+     */
+    public function getQuantityTens()
+    {
+        return $this->quantityTens;
+    }
+
+    /**
+     * 
+     * @param int $totalGames
+     * @return void
+     */
+    public function setTotalGames($totalGames)
+    {
+        $this->totalGames = $totalGames;
+    }
+
+    /**
+     * 
+     * @return int
+     */
+    public function getTotalGames()
+    {
+        return $this->totalGames;
+    }
+
+    /**
+     * 
+     * @param array $result
+     * @return void
+     */
+    public function setResult($result)
+    {
+        $this->result = $result;
+    }
+
+    /**
+     * 
+     * @return array
+     */
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+    /**
+     * 
+     * @param array $games
+     * @return void
+     */
+    public function setGames($games)
+    {
+        $this->games = $games;
+    }
+
+    /**
+     * 
+     * @return array
+     */
+    public function getGames()
+    {
+        return $this->games;
+    }
+
 }
